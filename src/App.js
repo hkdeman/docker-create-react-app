3ff164d524001533c93a2aea750aa4cd823cd0da
hkdeman <hemangkandwal@gmail.com>
2024-03-22 11:33:35 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to H!</h1>
         </header>
         <p className="App-intro">
-          To get started, message me on <a><code>social-media/linkedin.js</code></a>, and let's chat.
+          To get started, message me on <a><code>social-media/linkedin.js</code></a>, and let's chat!
         </p>
       </div>
     );
